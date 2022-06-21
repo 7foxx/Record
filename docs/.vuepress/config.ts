de6dcd1ff80df67245478cd15a3694f202fc0e58
Ali <951299034@qq.com>
2022-06-21 21:12:35 +0800

    添加编辑功能

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,7 @@ const { searchPlugin } = require('@vuepress/plugin-search')
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， 打工人 ！',
-  port:8082,
+  port: 8082,
   // description: '这是我的第一个 VuePress 站点',
   theme: defaultTheme({
     // 默认主题配置
@@ -21,10 +21,15 @@ export default defineUserConfig({
       {
         text: '面试题',
         link: '/page/TheInterview/A-Preface'
-      },
+      }
     ],
     sidebar: Sidebar(),
-    repo:"https://github.com/7foxx/Record'"
+    // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#git-%E4%BB%93%E5%BA%93%E5%92%8C%E7%BC%96%E8%BE%91%E9%93%BE%E6%8E%A5
+    repo: 'https://github.com/7foxx/Record',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'docs',
+    // 默认为 "Edit this page"
+    editLinkText: '在 GitHub 上编辑此页'
   }),
   plugins: [
     // 代码复制
@@ -39,13 +44,13 @@ export default defineUserConfig({
     searchPlugin({
       locales: {
         '/': {
-          placeholder: 'Search',
+          placeholder: 'Search'
         },
         '/zh/': {
-          placeholder: '搜索',
-        },
+          placeholder: '搜索'
+        }
       },
-      maxSuggestions: 10,
-    }),
+      maxSuggestions: 10
+    })
   ]
 })
