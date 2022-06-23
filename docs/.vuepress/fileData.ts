f37c58194bff4cf9d42e92d40028e280c19c198c
Ali <951299034@qq.com>
2022-06-23 16:00:18 +0800

    约定式路由2.0

--- docs/.vuepress/fileData.ts
+++ docs/.vuepress/fileData.ts
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const pathDir = require('path')
 
-;(() => {
+// 自动读取文件添加路由
+// 只支持到三级目录
+// 目录文件里不要方文件
+export default function getJsonFiles() {
   const arr: any = []
+  // 先获取所有文件夹名称
   function fileFun(path) {
     const files = fs.readdirSync(pathDir.resolve(__dirname, `../${path}`))
-    // console.log(files)
     for (let i = 0; i < files.length; i++) {
       const file = files[i]
       let fPath: string = pathDir.join(path, file)
@@ -30,7 +33,7 @@ const pathDir = require('path')
   fileFun('page')
   arr.shift()
 
-  let newArr = []
+  let newArr = [] as any
   for (let i = 0; i < arr.length; i++) {
     const ele = arr[i]
     const key = Object.keys(ele)[0]
@@ -45,86 +48,71 @@ const pathDir = require('path')
       }
     }
   }
-  // console.log(JSON.stringify(newArr));
-  // console.log(newArr);
-  
-})()
-
-throw 1
-
-// 自动读取文件添加路由
-export default function getJsonFiles() {
-  const fileArr: any = []
-  function fileFun(path) {
-    const files = fs.readdirSync(pathDir.resolve(__dirname, `../${path}`))
-    files.map(file => {
-      let fPath: string = pathDir.join(path, file)
-      let stat = fs.statSync(pathDir.resolve(__dirname, `../${fPath}`))
-      if (stat.isDirectory()) {
-        fileArr.push({
-          fileName: fPath
-        })
-        fileFun(fPath)
-      }
-      if (stat.isFile()) {
-        const arr: string[] = fPath.split('\\')
-        arr.pop() as string
-        const path = arr.join('\\')
-        for (let i = 0; i < fileArr.length; i++) {
-          for (let j = i; j < fileArr.length; j++) {
-            const item = fileArr[j]
-            if (path === item.fileName) {
-              if (item.children?.length) {
-                !item.children.includes(fPath) && item.children.push(fPath)
-              } else {
-                item.children = [fPath]
-              }
+  // 递归读取内容
+  function isValues(item) {
+    const values = Object.values(item)[0] as any
+    const key = Object.keys(item)[0] as any
+    values.map(path => {
+      if (typeof path === 'string') {
+        const files = fs.readdirSync(pathDir.resolve(__dirname, `../${path}`))
+        const index = values.findIndex(i => i === path)
+        item[key].some(e => {
+          if (e === path && typeof e === 'string') {
+            const children = files
+              .map(v => {
+                if (!/txt/.test(v)) {
+                  return `/${path}\\${v}`.replace(/\\/g, '/')
+                }
+              })
+              .filter(v => v)
+            // 将README文件放在第一个
+            const I = children.findIndex(e => /README/g.test(e))
+            const value = children.find(e => /README/g.test(e))
+            children.splice(I, 1)
+            children.unshift(value)
+            item[key][index] = {
+              text: files
+                .map(v => {
+                  if (/txt/.test(v)) {
+                    return v.replace(/\.txt/gi, '')
+                  }
+                })
+                .filter(v => v)[0],
+              children
             }
           }
+        })
+      } else {
+        isValues(path)
+      }
+    })
+  }
+  for (let i = 0; i < newArr.length; i++) {
+    const element = newArr[i]
+    isValues(element)
+  }
+  // 最后处理成路由
+  const router = {}
+  newArr.map(item => {
+    const keys = Object.keys(item)[0]
+    const values = Object.values(item)[0]
+    for (const key in item) {
+      const value = item[key]
+      for (let iterator of value) {
+        if (iterator['text']) {
+          iterator['collapsible'] = true
+        } else {
+          const kye = Object.keys(iterator)[0]
+          const text = kye.split('\\')
+          iterator['text'] = text[text.length - 1]
+          iterator['collapsible'] = true
+          iterator['children'] = Object.values(iterator)[0]
+          delete iterator[kye]
         }
       }
-    })
-  }
-  // 从 page 文件夹开始
-  fileFun('page')
-  const obj = {}
-  const newFileArr = fileArr.filter(item => {
-    if (item.children) {
-      const arr = item.children.find(v => /txt/gi.test(v)).split('\\')
-      item.text = arr[arr.length - 1].replace(/.txt/gi, '')
-      item.children = item.children.filter(v => {
-        if (!/txt/gi.test(v)) return v
-      })
-      return item
-    } else {
-      obj[`/${item.fileName.replace(/\\/gi, '/')}/`] = []
     }
+    router[`/${keys.replace(/\\/g, '/')}/`] = values
   })
-  newFileArr.map(item => {
-    for (const key in obj) {
-      const arr: string[] = item?.fileName?.split('\\')
-      arr?.pop() as string
-      const path = arr?.join('/')
-      if (key === `/${path}/`) {
-        delete item.fileName
-        item.children = item.children.map(e => `/${e.replace(/\\/gi, '/')}`)
-        // 折叠展开属性
-        item['collapsible'] = true
-        obj[key].push(item)
-      }
-    }
-  })
-  for (const key in obj) {
-    const item = obj[key]
-    item.map(v => {
-      if (v.children?.length) {
-        const val = v.children.find(e => /README/gi.test(e))
-        const arr = v.children.filter(e => !/README/gi.test(e))
-        arr.unshift(val)
-        v.children = arr
-      }
-    })
-  }
-  // console.log(JSON.stringify(obj));
-  return obj
+  // console.log(JSON.stringify(router))
+  return router
 }
